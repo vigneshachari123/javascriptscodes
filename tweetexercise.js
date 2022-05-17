448ab45085321425c0a80792876b5369bcd055fc
vigneshachari123 <105626005+vigneshachari123@users.noreply.github.com>
2022-05-17 12:46:51 +0530

    Create tweetexercise.js

---
+++ tweetexercise.js
@@ -0,0 +1,4 @@
+// Limit the tweet length only upto 100;
+var tweet = prompt("Compose your tweet");
+var tweetcount= tweet.length;
+alert("You have written "+tweetcount+ " characters"+ "   "+(100-tweetcount)+" "+" characters left in your tweet");
